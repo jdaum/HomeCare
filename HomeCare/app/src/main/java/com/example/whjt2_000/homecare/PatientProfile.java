905cd0bd2224e8722b01a8a17f64319d0f75f4ba
jdaum <jdaum@cs.washington.edu>
2016-01-27 06:07:41 +0100

    stock answers first version

--- HomeCare/app/src/main/java/com/example/whjt2_000/homecare/PatientProfile.java
+++ HomeCare/app/src/main/java/com/example/whjt2_000/homecare/PatientProfile.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class PatientProfile extends AppCompatActivity {
+
+    ArrayList<String> list;
+    MyCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +23,36 @@ public class PatientProfile extends AppCompatActivity {
         setContentView(R.layout.activity_patient_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //generate list
+        list = new ArrayList<String>();
+        list.add("The patient was asleep.");
+        list.add("Normal Heartrate.");
+
+        Button add_btn= (Button)findViewById(R.id.add_btn);
+
+        //instantiate custom adapter
+        adapter = new MyCustomAdapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.my_listview);
+        lView.setAdapter(adapter);
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // this line adds the data of your EditText and puts in your array
+                list.add("Additional stock answer");
+                // next thing you have to do is check if your adapter has changed
+                adapter.notifyDataSetChanged();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
 }
