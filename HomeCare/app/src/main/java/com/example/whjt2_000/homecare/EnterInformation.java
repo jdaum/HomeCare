24f610f8993430f9205c4b0fa33a62bcd9022552
jiangj4 <jjiaying.ing@gmail.com>
2016-01-28 20:50:20 -0800

    enter info voice input

--- HomeCare/app/src/main/java/com/example/whjt2_000/homecare/EnterInformation.java
+++ HomeCare/app/src/main/java/com/example/whjt2_000/homecare/EnterInformation.java
@@ -1,13 +1,48 @@
 package com.example.whjt2_000.homecare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class EnterInformation extends AppCompatActivity {
+
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            EditText notes = (EditText) this.findViewById(R.id.editText);
+            notes.setText(notes.getText() + spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +50,15 @@ public class EnterInformation extends AppCompatActivity {
         setContentView(R.layout.activity_enter_information2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ImageButton voice_btn = (ImageButton)findViewById(R.id.button1);
+        voice_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                displaySpeechRecognizer();
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
