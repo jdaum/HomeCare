2d377081cc132335a3858e5c633c17b975182721
jdaum <jdaum@cs.washington.edu>
2016-02-04 07:28:06 +0100

    add checkbox

--- HomeCare/app/src/main/java/com/example/whjt2_000/homecare/EnterInformation.java
+++ HomeCare/app/src/main/java/com/example/whjt2_000/homecare/EnterInformation.java
@@ -1,5 +1,6 @@
 package com.example.whjt2_000.homecare;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -8,14 +9,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -27,6 +36,7 @@ public class EnterInformation extends AppCompatActivity {
     private DatabaseHelper  dbHelper;
     private String spokenText;
     private Spinner spinner;
+    private MyCustomCheckboxAdapter dataAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +61,11 @@ public class EnterInformation extends AppCompatActivity {
                 displaySpeechRecognizer();
             }
         });
+
+
+        //Generate list View from ArrayList
+        displayListView();
+
 
     }
 
@@ -104,7 +119,90 @@ public class EnterInformation extends AppCompatActivity {
         long rowId = dbHelper.addPatientInformation(bodysystem, message);
 
         //TODO: DEBUGGING ONLY
-        Log.d("EnterInformation","Databaserow: " +rowId);
+        Log.d("EnterInformation", "Databaserow: " + rowId);
 
     }
+
+    private void displayListView() {
+
+        //Array list of countries
+        ArrayList<String> countryList = new ArrayList<String>();
+        countryList.add("Test1");
+        countryList.add("Test2");
+        countryList.add("Test3");
+
+
+        //create an ArrayAdaptar from the String Array
+        dataAdapter = new MyCustomCheckboxAdapter(this,
+                R.layout.custom_checkboxlist, countryList);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                String stockanswer = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Clicked on Row: " + stockanswer,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    private class MyCustomCheckboxAdapter extends ArrayAdapter {
+
+        private ArrayList<String> stockanswers;
+
+        public MyCustomCheckboxAdapter(Context context, int textViewResourceId,
+                                       ArrayList<String> stockanswers) {
+            super(context, textViewResourceId, stockanswers);
+            this.stockanswers = new ArrayList<String>();
+            this.stockanswers.addAll(stockanswers);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.custom_checkboxlist, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener( new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v ;
+                        String country = (String) cb.getTag();
+                        Log.d("CustomCheckbox:", "is" + cb.getTag());
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            String stockanswer = stockanswers.get(position);
+            holder.code.setText(" (" + stockanswer + ")");
+            holder.name.setText(stockanswer);
+            return convertView;
+
+        }
+    }
+
 }
