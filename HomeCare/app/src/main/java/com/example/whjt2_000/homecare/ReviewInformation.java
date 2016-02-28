aa147b613eddc4abfdc52a6222d51006688554b0
jdaum <jdaum@cs.washington.edu>
2016-02-29 00:37:45 +0100

    fix calender and date selection

--- HomeCare/app/src/main/java/com/example/whjt2_000/homecare/ReviewInformation.java
+++ HomeCare/app/src/main/java/com/example/whjt2_000/homecare/ReviewInformation.java
@@ -11,6 +11,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.DateTimeKeyListener;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -27,9 +28,11 @@ public class ReviewInformation extends AppCompatActivity {
     int startYear, startMonth, startDay;
     boolean setStartDay, setEndDay;
     int endYear, endMonth, endDay;
-    TextView startText, endText;
+    private TextView startText, endText, showStartText, showEndText;
     private DatePickerDialog startDateDialog;
     private DatePickerDialog endDateDialog;
+
+    private SimpleDateFormat dateFormatter;
 
 
     @Override
@@ -41,51 +44,69 @@ public class ReviewInformation extends AppCompatActivity {
 
         startText = (TextView) findViewById(R.id.StartText);
         endText = (TextView) findViewById(R.id.EndText);
-
-        setStartDateDialog();
-        setEndDateDialog();
+        showStartText = (TextView) findViewById(R.id.showStartDate);
+        showEndText = (TextView) findViewById(R.id.showEndDate);
 
         setEndDay = setStartDay = false;
 
-        Button startButton = (Button) findViewById(R.id.StartButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
+
+        final Button startdate = (Button) findViewById(R.id.StartButton);
+        Button enddate = (Button) findViewById(R.id.EndButton);
+
+        startdate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                startYear = c.get(Calendar.YEAR);
+                startMonth = c.get(Calendar.MONTH);
+                startDay = c.get(Calendar.DAY_OF_MONTH);
+
+                //launch date picker
+
+                startDateDialog = new DatePickerDialog(ReviewInformation.this,
+                        new DatePickerDialog.OnDateSetListener(){
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // Display Selected date in textbox
+                                showStartText.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+                                setStartDay=true;
+                                startYear = year;
+                                startMonth = monthOfYear;
+                                startDay = dayOfMonth;
+                            }
+                        }, startYear, startMonth, startDay);
                 startDateDialog.show();
-                startDateDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (which == DialogInterface.BUTTON_POSITIVE) {
-                            Calendar date = Calendar.getInstance();
-//                            startYear = date.get(Calendar.YEAR);
-//                            startMonth = date.get(Calendar.MONTH) + 1;
-//                            startDay = date.get(Calendar.DAY_OF_MONTH);
-//                            setStartDay = true;
-                            setStartDate();
-                        }
-                    }
-                });
             }
         });
 
+        enddate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                endYear = c.get(Calendar.YEAR);
+                endMonth = c.get(Calendar.MONTH);
+                endDay = c.get(Calendar.DAY_OF_MONTH);
 
+                //launch date picker
 
-        Button endButton = (Button) findViewById(R.id.EndButton);
-        endButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+                endDateDialog = new DatePickerDialog(ReviewInformation.this,
+                        new DatePickerDialog.OnDateSetListener(){
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // Display Selected date in textbox
+                                showEndText.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+                                setEndDay=true;
+                                endYear = year;
+                                endMonth = monthOfYear;
+                                endDay = dayOfMonth;
+                            }
+                        }, endYear, endMonth, endDay);
                 endDateDialog.show();
-                endDateDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (which == DialogInterface.BUTTON_POSITIVE) {
-                                Calendar date = Calendar.getInstance();
-                                endYear = date.get(Calendar.YEAR);
-                                endMonth = date.get(Calendar.MONTH) + 1;
-                                endDay = date.get(Calendar.DAY_OF_MONTH);
-                                setEndDay = true;
-                            setEndDate();
-                        }
-                    }
-                });
             }
         });
+
 
         Button reviewButton = (Button) findViewById(R.id.ReviewButton);
         reviewButton.setOnClickListener( new View.OnClickListener(){
@@ -96,96 +117,32 @@ public class ReviewInformation extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void setStartDateDialog(){
-        Calendar c = Calendar.getInstance();
-        startDateDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Calendar date = Calendar.getInstance();
-                date.set(year, monthOfYear, dayOfMonth);
-                startYear = view.getYear();
-                startMonth = view.getMonth();
-                startDay = view.getDayOfMonth();
-                setStartDay = true;
-//                startYear = date.get(Calendar.YEAR);
-//                startMonth =  date.get(Calendar.MONTH) + 1;
-//                startDay = date.get(Calendar.DAY_OF_MONTH);
-//                setStartDay = true;
-            }
-        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
-    }
-
-    private void setEndDateDialog(){
-        Calendar c = Calendar.getInstance();
-        endDateDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Calendar date = Calendar.getInstance();
-                date.set(year, monthOfYear, dayOfMonth);
-//                endYear = date.get(Calendar.YEAR);
-//                endMonth =  date.get(Calendar.MONTH) + 1;
-//                endDay = date.get(Calendar.DAY_OF_MONTH);
-//                setEndDay = true;
-            }
-        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
-    }
-
-    public void setStartDate(){
-
-        if(setEndDay && !checkDate()){
-            Toast.makeText(this, "Please select an end date before " +
-                            new StringBuilder().append(endDay).append("/").append(endMonth).append("/").append(endYear).append("."),
-                    Toast.LENGTH_SHORT).show();
-        }
-        else {
-            setStartDay = true;
-            Toast.makeText(this, "Selected " + new StringBuilder().append(startDay).append("/").append(startMonth).append("/").append(startYear) + " as start date.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        startText.setText(startText.getText() + "\n" + new StringBuilder().append(startDay).append("/").append(startMonth).append("/").append(startYear));
-    }
-
-    public void setEndDate(){
-        if (setStartDay && !checkDate()) {
-            Toast.makeText(this, "Please select an end date after " +
-                            new StringBuilder().append(startDay).append("/").append(startMonth).append("/").append(startYear).append("."),
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            setEndDay = true;
-            Toast.makeText(this, "Selected " + new StringBuilder().append(endDay).append("/").append(endMonth).append("/").append(endYear) + " as end date.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        endText.setText(endText.getText() + "\n" + new StringBuilder().append(endDay).append("/").append(endMonth).append("/").append(endYear));
-    }
-
     private boolean checkDate(){
         if (endYear < startYear)
             return false;
-        else if(endYear > startYear)
-            return true;
-        if (endMonth < startMonth) return false;
-        else if(endMonth > startMonth) return true;
-
-        if (endDay < startDay) return false;
-        else return true;
+        if (endMonth < startMonth)
+            return false;
+        if (endDay < startDay)
+            return false;
+        return true;
     }
 
     public void enterInformation(View view){
-        if(!setStartDay || !setEndDay)
-            Toast.makeText(this, "Please select valid start and end date.", Toast.LENGTH_SHORT).show();
-        else {
-
+        if(setStartDay && setEndDay && checkDate()){
             Intent intent = new Intent(this, ReviewDate.class);
             intent.putExtra("startYear", startYear);
-            intent.putExtra("startMonth", startMonth);
+            intent.putExtra("startMonth", startMonth+1);
             intent.putExtra("startDay", startDay);
 
             intent.putExtra("endYear", endYear);
-            intent.putExtra("endMonth", endMonth);
+            intent.putExtra("endMonth", endMonth+1);
             intent.putExtra("endDay", endDay);
 
 
             startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Please select valid start and end date.", Toast.LENGTH_SHORT).show();
         }
     }
 
